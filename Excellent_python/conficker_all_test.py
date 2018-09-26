484beb6edc6c29f5d654a696101a43bf7f56644c
root <root@DESKTOP-KA95HG1.localdomain>
2018-09-27 01:49:44 +0800

    add conficker_all_test

---
+++ Excellent_python/conficker_all_test.py
@@ -0,0 +1,49 @@
+#
+
+import os
+import optparse
+import sys
+import nmap
+
+def findtarget()
+	
+
+
+
+def setuphandler()
+
+
+
+
+def confickerexploit()
+
+
+
+
+def smbbrute()
+
+
+
+
+def main()
+	configfile = open('meta.rc','w')
+	parser = optparse.OptionParser('[-] Usage%prog '+'-h <RHOST[s]> -l <LHOST> -p <LPORT> -f<password file>')
+	parser.add_option('-h',dest='targethost',type='string',help='specify the target host')
+	parser.add_option('-l',dest='lhost',type='string',help='local host')
+	parser.add_option('-p',dest='lport',type='string',help='local port')
+	parser.add_option('-f',dest='passwordfile',type='string',help='password file')
+	       
+
+	targethosts=findtarget(targethost)
+	setuphandler(configfile,lhost,lport)
+	confickerexploit(
+	smbbrute()
+	
+
+	
+
+
+
+if _name_ == '_main_':
+	main()
+

a283516a5f8ccc2f24357eabeb781060003e75d0
minyun168 <minyun168@outlook.com>
2018-11-21 22:11:44 +0800

    Update netcat.py

--- Excellent_python/netcat.py
+++ Excellent_python/netcat.py
@@ -129,6 +129,54 @@ def run_command(command):
 	#
 	return output
 
+def client_handler(client_socket):
+	global upload
+	global execute
+	global command
+	#
+	if len(upload_destination):
+		#
+		file_buffer = ""
+
+		#
+		while True:
+			data = client_socke.recv(1024)
+			 if not data:
+			 	break
+			 else:
+			 	file_buffer +=data
+
+		try:
+			file_descriptor = open(upload_destinaton,"wd")
+			file_descriptor.write(file_buffer)
+			file_descriptor.close()
+
+			#
+			client_socket.send("Successfully saved file to %s\r\n" %upload_destination)
+		except:
+			client_socket.send("Failed to save file to %s\r\n" % upload_destination)
+
+
+	if len(execute):
+
+		output = run_command(execute)
+		client_socket.send(output)
+
+	if command:
+		while True:
+
+			client_socket.send("<BHP:#> ")
+
+			cmd_buffer = ""
+			while "\n" not in cmd_buffer:
+				cmd_buffer += client_socket.recv(1024)
+				response = run_command(cmd_buffer)
+
+				client_socket.send(response)
+				
+
+
+
 
 
 

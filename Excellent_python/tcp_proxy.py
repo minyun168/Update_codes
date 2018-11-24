030c16d10590320450f6af238baed6011a5241e7
minyun168 <minyun168@outlook.com>
2018-11-24 22:31:35 +0800

    Create tcp_proxy.py

---
+++ Excellent_python/tcp_proxy.py
@@ -0,0 +1,26 @@
+#
+
+import sys
+import socket
+imoort threading
+
+sef server_loop(local_host,local_port,remote_host,remote_port,receive_first):
+	
+	server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+	try:
+		server.bind((local_host,local_port))
+	except:
+		print "[!!]Failed to listem on %s:%d" %(local_host, local_port)
+		print "[!!] check for other listening sockets or correct permission."
+		sys.exit(0)
+
+	print "[*] Listening on %s:%d" %(local_host,local_port)
+
+	srver.listem(5)
+
+	while TRue:
+
+		client_socket, addr = server.accept()
+		print "[==>+] Received incoming connection from %s:%dd" %(addr[0],addr[1])
+		proxy_thread = threading.Threading(target=proxy_handler,arg=(lient_socket,remote_port,receive_first))

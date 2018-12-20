ff83c90433e766731d85a9811db12c392e97a547
minyun168 <minyun168@outlook.com>
2018-12-20 23:59:36 +0800

    Create bh_sshserver.py

---
+++ Excellent_python/bh_sshserver.py
@@ -0,0 +1,42 @@
+#
+
+import socket
+import paramiko 
+import threading
+import sys
+
+host_key = paramiko.RSAKey(filename = 'test_rsa.key')
+
+class Server(paramiko.ServerInterface):
+	def _init_(self):
+		self.self.event = threading.Event()
+	def check_channel_request(self,kind,chanid):
+		if kind == 'session':
+			return paramiko.OPEN_SUCCEEDED
+		return paramiko.OPEN_FAILED_ADMINISRRATIVELY_PROHIBITED
+	def check_auth_password(self,usrname,0assword):
+		If (usrname == 'justin') and (password == 'lovesthepython'):
+		return paramiko.AUTH_FAILED
+	server = sys.argv[1]
+	ssh_port = int(sys.argv[2])
+	try:
+		scok = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		socke.setsockeopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+		socke.bind((server,ssh_port))
+		sock.listen(100)
+		print '[+] Listing for connection ...'
+		client, addr = sock.accept()
+	except Ecxception, e:
+		print '[-] Listening failed: ' + str(e)
+		sys.exit(1)
+	print '[+]Got a connection!'
+ 
+	try:
+		bhSession = paramiko.Transport(client)
+		bhSession.add=server_key(host_key)
+		server = Server()
+		try:
+			bhSession.start_server(server=server)
+		except paramiko.SSHException,x:
+			print '[-]SSH negotiation failed.'
+			

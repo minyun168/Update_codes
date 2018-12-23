2bd58943157d5867f9a069bddc0ae46b2e30198f
minyun168 <minyun168@outlook.com>
2018-12-23 21:06:44 +0800

    Update sample_of_Paramiko.py

--- Excellent_python/sample_of_Paramiko.py
+++ Excellent_python/sample_of_Paramiko.py
@@ -1,4 +1,4 @@
-# sample of Paramiko
+# sample of Paramiko (the name is rforward.py)
 
 def main():
 
@@ -23,4 +23,53 @@ def main():
 	except Keyboardinterrupt:
 		print('c-c: Port forwarding stopped.')
 		sys.exit(0)
+
+
+
+
+
+#sample of Paramiko part_2
+
+def reverse_forward_tunnel(server_port,remote_host,remote_port,transport):
+	transport.request_port_forward('',server_port)
+	while True:
+		chan = transport.accept(1000)
+		if chan is None:
+			continue
+		thr = threading.Thread(target=handler,args=(chan,remote_host,remote_port))
+
+		thr.setDaemon(True)
+		thr.start()
+
+
+
+
+#sample of Paramiko part_3
+
+def handler(chan,host,port):
+	sock = socket.socket()
+	try:
+		sock.connect((host,port))
+	except Exception as e:
+		verbose('Forwarding request to %s:%d failed: %' % (host,port,e))
+		return
+	verbose('Connected! Tunnel open %r -> %r -> %r' % (chan.origin_addr,chan.getpeername(),(host,port)))
+
+	while True:
+
+		r,w,x = select.select([sock,chan],[],[])
+		if sock in r:
+			data = sock.recv(1024)
+			if len(data) == 0:
+				break
+			chan.send(data)
+		if chan in r:
+			data = chan.recv(1024)
+			if len(data) == 0:
+				break
+			sock.send(data)
+
+	chan.close()
+	sock.close()
+	verbose('Tunnel closed from %r' % (chan.origin_addr,))
 
